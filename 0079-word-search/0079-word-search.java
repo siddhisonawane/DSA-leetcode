986049d523de092a1ea7e2f68941600274483205
Siddhi Uttam Sonawane <siddhisonawane2004@gmail.com>
2025-07-06 22:14:53 +0530

    Time: 133 ms (78.82%), Space: 42 MB (25.6%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m=board.length;
+        int n=board[0].length;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(dfs(board,word,i,j,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean dfs(char[][] board,String word,int i,int j,int index){
+        if(index==word.length()){
+            return true;
+        }
+
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j]!=word.charAt(index)){
+                return false;
+        }
+        char temp = board[i][j];
+        board[i][j] = '#';
+          boolean found = dfs(board, word, i + 1, j, index + 1) ||
+                         dfs(board, word, i - 1, j, index + 1) ||
+                         dfs(board, word, i, j + 1, index + 1) ||
+                         dfs(board, word, i, j - 1, index + 1);
+        board[i][j]=temp;
+
+        return found;
+    }
+    public static void main(String args[]){
+        Solution ws = new Solution();
+        char board[][]={{'A','B','C','E'},
+                        {'S','F','C','S'},
+                        {'A','D','E','E'}};
+        String word="ABCCED";
+
+        System.out.println(ws.exist(board,word));
+    }
+}
