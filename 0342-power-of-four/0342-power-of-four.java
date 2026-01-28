10cbbf9f6a35bde7aa6abb2f18601492c56699b0
Siddhi Uttam Sonawane <siddhisonawane2004@gmail.com>
2026-01-28 19:19:06 +0530

    Time: 1 ms (88.81%), Space: 43 MB (9.59%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        
+        while (n % 4 == 0) {
+            n /= 4;
+        }
+
+        return n == 1;
+
+    }
+    public static void main(String args[]){
+        int n=16;
+
+        System.out.println(isPowerOfFour(n));
+    }
+}
